1a07c37b77ebf2fd89b5780fa1aa9a6580e09c6d
alexander.berkovich <alexander.berkovich@pursway.com>
2019-12-03 16:51:09 +0200

    Dravt version completed

---
+++ Python/Linux/check1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+def main():
+    print("Hello World!")
+    print("Python version is")
+    print(3,)
+    print(sys.version_info>=(3,))
+
+
+if __name__ == "__main__":
+   main()
